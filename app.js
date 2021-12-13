48d0d844789ac5389648fea0f04bcc56ed2b0cc4
alejoAliberti <alibertialejo56@gmail.com>
2021-12-13 11:20:24 -0300

    trabajo mercadoliebre

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo en el puerto 3030');
+});
+
+app.get('/', (require, response) => {
+    response.sendFile(path.resolve(__dirname, 'views/home.html'));
+});
+
+app.get('/registro', (require, response) => {
+    response.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.get('/login', (require, response) => {
+    response.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.use(express.static('public'));
+
+
+
+
+
+
